3d5049404a73d18e8134e6d4173c542766ee817c
IDR-WORKS\admin <seunghyun87.ryu@gmail.com>
2020-12-21 14:02:40 +0900

    작업 중

---
+++ js/lnb_data_works.js
@@ -0,0 +1,22 @@
+/**
+ * LnbNav Methods
+ */
+lnbInfoData = {
+    lnb_d1: [
+        { num:"lnb1_09", txt:"2021", url:"javascript:forWroking();", state:"" },
+        {
+            num:"lnb1_08", txt:"2020", url:"works_2020_001.html", state:"",
+            lnb_d2:[
+                {num:"lnb2_01", txt:"1", url:"works_2020_001.html", state:""},
+                {num:"lnb2_02", txt:"2", url:"works_2020_002.html", state:""},
+            ]
+        },
+        { num:"lnb1_07", txt:"2019", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_06", txt:"2018", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_05", txt:"2017", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_04", txt:"2016", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_03", txt:"2015", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_02", txt:"2014", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_01", txt:"2013", url:"javascript:forWroking();", state:"" }
+    ]
+};
