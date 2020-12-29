4f7e1973969ca60fd4259c7681471862177f9843
IDR-WORKS\admin <seunghyun87.ryu@gmail.com>
2020-12-29 14:35:13 +0900

    babel 추가

---
+++ dev/js_dev/lnb_data_works.js
@@ -0,0 +1,43 @@
+/**
+ * LnbNav Methods
+ */
+const lnbInfoData = {
+    lnb_d1: [
+        {
+            num:"lnb1_20", txt:"2020", url:"works_2020_01.html", state:"",
+            lnb_d2:[
+                {num:"lnb2_01", txt:"1", url:"works_2020_01.html", state:""},
+                {num:"lnb2_02", txt:"2", url:"works_2020_02.html", state:""},
+            ]
+        },
+        { num:"lnb1_19", txt:"2019", url:"works_2019_01.html", state:"",
+            lnb_d2:[
+                {num:"lnb2_01", txt:"1", url:"works_2019_01.html", state:""},
+                {num:"lnb2_02", txt:"2", url:"works_2019_02.html", state:""},
+                {num:"lnb2_03", txt:"3", url:"works_2019_03.html", state:""},
+            ]
+        },
+        { num:"lnb1_17", txt:"2017", url:"works_2017_01.html", state:"",
+            lnb_d2:[
+                {num:"lnb2_01", txt:"1", url:"works_2017_01.html", state:""},
+                {num:"lnb2_02", txt:"2", url:"works_2017_02.html", state:""},
+            ]
+        },
+        { num:"lnb1_16", txt:"2016", url:"works_2016_01.html", state:"",
+            lnb_d2:[
+                {num:"lnb2_01", txt:"1", url:"works_2016_01.html", state:""},
+                {num:"lnb2_02", txt:"2", url:"works_2016_02.html", state:""},
+                {num:"lnb2_03", txt:"3", url:"works_2016_03.html", state:""},
+            ]
+        },
+        { num:"lnb1_15", txt:"2015", url:"works_2015_01.html", state:"",
+            lnb_d2:[
+                {num:"lnb2_01", txt:"1", url:"works_2015_01.html", state:""},
+                {num:"lnb2_02", txt:"2", url:"works_2015_02.html", state:""},
+                {num:"lnb2_03", txt:"3", url:"works_2015_03.html", state:""},
+            ]
+        },
+        { num:"lnb1_14", txt:"2014", url:"javascript:forWroking();", state:"" },
+        { num:"lnb1_13", txt:"2013", url:"javascript:forWroking();", state:"" }
+    ]
+};
